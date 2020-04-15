0ab566177a12cf2a60d85d2dd51ed139d6ccf221
oblivioned <371397938@qq.com>
2020-04-15 12:58:32 +0800

    import project
    base of webview_flutter 0.3.19+9
    
    https://github.com/flutter/plugins/tree/master/packages/webview_flutter

---
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/apple/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/apple/.pub-cache/hosted/pub.flutter-io.cn/webview_flutter-0.3.19+9/example"
+export "FLUTTER_TARGET=/Users/apple/.pub-cache/hosted/pub.flutter-io.cn/webview_flutter-0.3.19+9/example/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "OTHER_LDFLAGS=$(inherited) -framework Flutter"
+export "FLUTTER_FRAMEWORK_DIR=/Users/apple/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "TRACK_WIDGET_CREATION=true"
